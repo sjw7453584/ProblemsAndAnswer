e1051c32768ebaa17bb719beaaea4c21b480f3df
sjw <526412568@qq.com>
2014-05-23 23:48:07 +0800

    fix a bug in createPrj.sh

--- createPrj.sh
+++ createPrj.sh
@@ -9,7 +9,7 @@ echo "(if (file-exists-p \"$curDir/${referFile}\")
 (ede-cpp-root-project \"$projectName\" :file \"$curDir/${referFile}\"
 					  :include-path '( 
 									   " >>~/_emacs/projects.el
-find -iname "*.h"|sed -n 's/\/[-a-zA-Z0-9_]*\.h//'p |sort |uniq |sed -n 's/\.\//"\//'p |sed -n 's/[-\"\/a-zA-Z_]*/&\/"/'p >>~/_emacs/projects.el
+find -iname "*.h"|sed -n 's/\/[-a-zA-Z0-9_]*\.h//'p |sort |uniq |sed -n 's/\.\//"\//'p |sed -n 's/[-\"\/a-zA-Z_0-9\.]*/&\/"/'p >>~/_emacs/projects.el
 
 echo ")
 :system-include-path '( \"/usr/include/\" \"/usr/lib/gcc/x86_64-linux-gnu/4.6/include/\" \"/usr/local/include/\" \"/usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/\" \"/usr/include/x86_64-linux-gnu/\" \"/usr/include/c++/4.6/\" \"/usr/include/c++/4.6/x86_64-linux-gnu/\" \"/usr/include/c++/4.6/backward/\" \"/usr/lib/syslinux/com32/include/\")
