e101d9f6d858d4738bbc15d221ae74e3d8c3cc3c
media <media@media-SMBIlly.(none)>
2015-05-30 11:35:57 +0800

    auto include system include path

--- createPrj.sh
+++ createPrj.sh
@@ -5,14 +5,23 @@ if [ "$referFile" = "" ];then
 fi 
 curDir=`pwd`
 projectName=${curDir##*\/}
+main=createPrj_test_main.cpp
+gccInfo=createPrj_test_gccInfo
+
+echo "int main(){return 0;}">$main
+gcc -v $main 2>$gccInfo
+sysInclude=`sed -n "/#include <\.\.\.> search starts here:/,/End of search list/"'p' gcc.out|grep -v "search"|sed -n 's/[ \t]*//'p|sed -n 's/[-\"\/a-zA-Z_0-9\.+]*/\\"&\\"/'p`
+
 echo "(if (file-exists-p \"$curDir/${referFile}\")
 (ede-cpp-root-project \"$projectName\" :file \"$curDir/${referFile}\"
 					  :include-path '( 
 									   " >>~/_emacs/projects.el
-find -iname "*.h"|sed -n 's/\/[-a-zA-Z0-9_]*\.h//'p |sort |uniq |sed -n 's/\.\//"\//'p |sed -n 's/[-\"\/a-zA-Z_0-9\.]*/&\/"/'p >>~/_emacs/projects.el
+find -iname "*.h"|sed -n 's/\/[-a-zA-Z0-9_+]*\.h//'p |sort |uniq |sed -n 's/\.\//"\//'p |sed -n 's/[-\"\/a-zA-Z_0-9\.]*/&\/"/'p >>~/_emacs/projects.el
 
 echo ")
-:system-include-path '( \"/usr/include/\" \"/usr/lib/gcc/x86_64-linux-gnu/4.6/include/\" \"/usr/local/include/\" \"/usr/lib/gcc/x86_64-linux-gnu/4.6/include-fixed/\" \"/usr/include/x86_64-linux-gnu/\" \"/usr/include/c++/4.6/\" \"/usr/include/c++/4.6/x86_64-linux-gnu/\" \"/usr/include/c++/4.6/backward/\" \"/usr/lib/syslinux/com32/include/\")
+:system-include-path '( $sysInclude )
 					  )
 )" >>~/_emacs/projects.el
+
+rm $main $gccInfo
 
